e0739417007b1cbea956a4943cd325ddd9174a77
harimaux <lukaszwitek20@googlemail.com>
2022-04-21 21:56:46 +0100

    gallery finished

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ document.querySelector("#navBtn").addEventListener("click", () => {
   document.querySelector("nav ul").classList.toggle("navToggle");
   document.querySelector("#navBtn").classList.toggle("navBtnColorToggle");
 });
+
+lightGallery(document.getElementById("lightgallery"), {
+  plugins: [lgZoom, lgThumbnail],
+  licenseKey: "free/non comercial",
+  speed: 500,
+});
