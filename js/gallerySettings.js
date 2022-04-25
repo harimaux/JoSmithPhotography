6258f97276d27a53bc9cd83b790de2ce3036dab2
harimaux <lukaszwitek20@googlemail.com>
2022-04-25 16:42:19 +0100

    testimonials page done

---
+++ js/gallerySettings.js
@@ -0,0 +1,5 @@
+lightGallery(document.getElementById("lightgallery"), {
+  plugins: [lgZoom, lgThumbnail],
+  licenseKey: "free/non comercial",
+  speed: 500,
+});
--- js/script.js
+++ js/script.js
@@ -3,8 +3,20 @@ document.querySelector("#navBtn").addEventListener("click", () => {
   document.querySelector("#navBtn").classList.toggle("navBtnColorToggle");
 });
 
-lightGallery(document.getElementById("lightgallery"), {
-  plugins: [lgZoom, lgThumbnail],
-  licenseKey: "free/non comercial",
-  speed: 500,
+const tesText1Box = document.querySelector(".tesTextAnime1");
+const tesText2Box = document.querySelector(".tesTextAnime2");
+
+window.addEventListener("scroll", () => {
+  const tesText1 = tesText1Box.getBoundingClientRect().top;
+  const tesText2 = tesText2Box.getBoundingClientRect().top;
+
+  const objectOnScreenPosition = window.innerHeight / 1.5;
+
+  if (tesText1 < objectOnScreenPosition) {
+    tesText1Box.classList.remove("tesTextAnime1");
+  }
+
+  if (tesText2 < objectOnScreenPosition) {
+    tesText2Box.classList.remove("tesTextAnime2");
+  }
 });
