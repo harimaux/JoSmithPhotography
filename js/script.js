4be7165185d6df4408646d8fee3940d208300595
harimaux <lukaszwitek20@googlemail.com>
2022-04-26 00:50:24 +0100

    packages init

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
+// Nvigation
+
+/* const { $LG } = require("lightgallery/lgQuery"); */
+
 document.querySelector("#navBtn").addEventListener("click", () => {
   document.querySelector("nav ul").classList.toggle("navToggle");
   document.querySelector("#navBtn").classList.toggle("navBtnColorToggle");
 });
+
+//Shows text on testimonials site
 
 const tesText1Box = document.querySelector(".tesTextAnime1");
 const tesText2Box = document.querySelector(".tesTextAnime2");
@@ -20,3 +26,25 @@ window.addEventListener("scroll", () => {
     tesText2Box.classList.remove("tesTextAnime2");
   }
 });
+
+// Packages Cards
+
+$(".cards").hover(function () {
+  if ($(this).hasClass("card1")) {
+    $(".pacBackground").html("<img src='images/packages/pacBackground1.jpeg'>");
+  } else if ($(this).hasClass("card2")) {
+    $(".pacBackground").html("<img src='images/packages/pacBackground2.jpeg'>");
+  } else if ($(this).hasClass("card3")) {
+    $(".pacBackground").html("<img src='images/packages/pacBackground3.jpeg'>");
+  } else if ($(this).hasClass("card4")) {
+    $(".pacBackground").html("<img src='images/packages/pacBackground4.jpeg'>");
+  } else if ($(this).hasClass("card5")) {
+    $(".pacBackground").html("<img src='images/packages/pacBackground5.jpeg'>");
+  }
+});
+
+/* $(".cards").mouseleave(function () {
+  if ($(this).hasClass("cards")) {
+    $(".pacBackground").html("");
+  }
+}); */
