ff2bceb6865689c073ae99b666a66bc313866069
harimaux <lukaszwitek20@googlemail.com>
2022-04-28 19:00:26 +0100

    beta 2.0 - fixes

--- js/script.js
+++ js/script.js
@@ -7,19 +7,18 @@ document.querySelector("#navBtn").addEventListener("click", () => {
   document.querySelector("#navBtn").classList.toggle("navBtnColorToggle");
 });
 
-const navBox = document.querySelector("nav");
+/* const navBox = document.querySelector("nav");
 const logoBox = document.querySelector(".logo");
 
 window.addEventListener("scroll", () => {
   const navPosition = navBox.getBoundingClientRect().top;
 
-  console.log(navPosition);
 
   if (navPosition < 10) {
     navBox.classList.add("navAlter");
     logoBox.classList.add("logoHide");
   }
-});
+}); */
 
 // Packages Cards Background Slideshow
 
@@ -40,15 +39,27 @@ const contactBtn = document.querySelector("#btnContact");
 const modalContainer = document.querySelector(".conModalContainer");
 const modalBox = document.querySelector(".conModalShadow");
 
+//Show modal
 contactBtn.addEventListener("click", () => {
   modalContainer.classList.add("conModalContainerShow");
 });
 
+//Hide modal
 modalBox.addEventListener("click", () => {
   modalContainer.classList.remove("conModalContainerShow");
 });
 
 //Contact map
+
+let counter = 0;
 document.querySelector("#btnMap").addEventListener("click", () => {
   document.querySelector(".conMap").classList.toggle("conMapShow");
+
+  counter++;
+
+  if (counter % 2 == 0) {
+    window.scrollTo(0, 0);
+  } else {
+    window.scrollTo(0, document.body.scrollHeight);
+  }
 });
